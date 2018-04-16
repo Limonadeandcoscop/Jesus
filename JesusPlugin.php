1e0e63158c972e790123866929e77658bcfb459d
Franck Dupont <kyfr59@gmail.com>
2018-04-16 10:31:11 +0200

    First commit : initialize plugin

---
+++ JesusPlugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jesus
+ *
+ * Provide functionalities for Enfant Jesus project
+ *
+ * @copyright Copyright 2011-2012 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ * @package JesusPlugin
+ */
+
+define('JESUS_PLUGIN_DIR', dirname(__FILE__));
+
+require_once JESUS_PLUGIN_DIR . '/functions.php';
+
+/**
+ * The Participate plugin.
+ * @package Omeka\Plugins\Participate
+ */
+class JesusPlugin extends Omeka_Plugin_AbstractPlugin
+{
+
+    /**
+     * @var array Hooks for the plugin.
+     */
+    protected $_hooks = array(
+    );
+
+
+    /**
+     * @var array Filters for the plugin.
+     */
+    protected $_filters = array(
+        
+    );
+
+
+    public static function getMapping()
+    {
+        return parse_ini_file(JESUS_PLUGIN_DIR.'/mapping.ini', true);
+    }
+
+        
+
+
+
+
+}
+
+
+
+
